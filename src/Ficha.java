48d5434c26e393539f12f8edced8f9253a62adbc
BdKy95 <51023903+BdKy95@users.noreply.github.com>
2021-05-08 14:51:34 -0300

    Initial commit

---
+++ src/Ficha.java
@@ -0,0 +1,32 @@
+
+public class Ficha {
+
+	private int numero;
+	private Paisaje izquierda;
+	private Paisaje derecha;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Paisaje getIzquierda() {
+		return izquierda;
+	}
+
+	public void setIzquierda(Paisaje izquierda) {
+		this.izquierda = izquierda;
+	}
+
+	public Paisaje getDerecha() {
+		return derecha;
+	}
+
+	public void setDerecha(Paisaje derecha) {
+		this.derecha = derecha;
+	}
+
+}
---
+++ src/Paisaje.java
@@ -0,0 +1,23 @@
+
+public class Paisaje {
+	
+	private TipoDePaisaje tipo;
+	private int coronas;
+	
+	public TipoDePaisaje getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoDePaisaje tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getCoronas() {
+		return coronas;
+	}
+
+	public void setCoronas(int coronas) {
+		this.coronas = coronas;
+	}
+
+}
